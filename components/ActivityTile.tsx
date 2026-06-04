692c2cf6ea7331e831c18f65022c4ea8641c3161
SANCHIys <sharmasanchi027@gmail.com>
2026-06-04 12:43:07 +0530

    docs: ADDED GRADIENT

--- components/ActivityTile.tsx
+++ components/ActivityTile.tsx
@@ -1,7 +1,7 @@
 "use client"
 export default function ActivityTile(){
     const activityData = [1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0];    return(
-        <section className="grid grid-cols-10 gap-1 bg-gray-800 rounded-xl p-5 border bg-gradient-to-br from-gray-800 via-gray-900 to-violet-900 rounded-xl p-4 border border-gray-700">            
+        <section className="grid grid-cols-10 gap-1 bg-gray-800 rounded-xl p-5 border bg-gradient-to-br from-gray-800 to-indigo-900 rounded-xl p-4 border border-gray-700">            
             <h3 className="col-span-10">Month Activity!</h3>
             {activityData.map((daysActive, index) => (
                 <div 
